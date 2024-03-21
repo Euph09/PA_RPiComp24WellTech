9c3ce65475be8ddb18b03896150c3919b2224e25
Euph09 <143952136+Euph09@users.noreply.github.com>
2024-03-21 11:38:06 +0000

    Update OutdoorAirQuality.R
    Fixed bug with data missing

--- OutdoorAirQuality.R
+++ OutdoorAirQuality.R
@@ -5,37 +5,43 @@ library(openair)
 library(tidyverse)
 library(stringi)
 
-#Access DEFRA AURN data
 aurn_meta <- importMeta(source = "aurn", all = TRUE) #Import site data for aurn network
 
-#Filter sites by those that are currently measuring PM10, and are in a Rural Background
 pm10_sites <- filter(aurn_meta, 
                      variable == "PM10",
                      end_date =="ongoing",
                      site_type=="Rural Background") # Search for pm10 sites with ongoing action with rural background
 
-
 site <- importAURN(site = "HM", data_type = "24_hour", year = 2024) # Import data for specific site
 
 site<-site[rev(order(as.Date(site$date, format="%m/%d/%Y "))),] # Sort site data by most recent
 
-
-#Assign variables values for most recent PM2.5 an PM10
-pm25 <-site[1,6]
-pm10 <-site[1,5]
+#Searches for most recent data, at same time as currently, where there is data
 
 day <- format(Sys.time(), "%Y-%m-%d %H:00:00")
 
-if(nchar(mday(day)-1) <2){
-  yesterday <-paste("0",mday(day)-1, sep="")
-}else{
-  yesterday <-mday(day)-1
+found = FALSE;
+i<-1
+while(found == FALSE){
+  if(nchar(mday(day)-1) <2){
+    yesterday <-paste("0",mday(day)-i, sep="")
+  }else{
+    yesterday <-mday(day)-i
+  }
+  stri_sub(day,9,10)<-yesterday
+  
+  entry<-filter(site,
+                date==day)
+  pm25 <-entry[1,6]
+  pm10<-entry[1,5]
+  
+  
+  if(is.na(pm25)||is.na(pm10)){
+    found = FALSE
+  }else{
+    found = TRUE
+  }
+  i<-i+1
 }
-stri_sub(day,9,10)<-yesterday
-
-entry<-filter(site,
-              date==day)
-
-pm25 <-entry[1,6]
-pm10<-entry[1,5]
+  
 print(paste(pm25, pm10, sep=","))
