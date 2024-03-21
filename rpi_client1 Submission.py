daaa1a625b542e57bda7190d98ad20506301c39f
Euph09 <143952136+Euph09@users.noreply.github.com>
2024-03-21 10:41:49 +0000

    Update comments rpi_client1 Submission.py

--- rpi_client1 Submission.py
+++ rpi_client1 Submission.py
@@ -2,20 +2,24 @@ import re
 import socket
 import subprocess
 
+#Gather data from R for outdoor air quality
 command = "PATH_TO_R"
 path = "PATH_TO_SCRIPT"
 cmd = [command, path]
 
+#Execute command to run R script
 with subprocess.Popen(cmd, stdout=subprocess.PIPE) as proc:
     output = proc.stdout.read()
 
+#R output is encoded, so decode
 output = output.decode("utf-8")
 
+#Extract data we want
 data = re.findall(r'\".*?\"',output)[0]
 print(f"The PM data is {data}")
 
 
-
+#Send data over to RPi, via sockets
 s =socket.socket()
 
 hostname='ENTER_IP'
