fb93eae64ce174efd3c1a4fe5259d206353ee0a4
Euph09 <143952136+Euph09@users.noreply.github.com>
2024-03-21 10:58:56 +0000

    Update OutdoorAirQuality.R

--- OutdoorAirQuality.R
+++ OutdoorAirQuality.R
@@ -5,12 +5,15 @@ library(openair)
 library(tidyverse)
 library(stringi)
 
+#Access DEFRA AURN data
 aurn_meta <- importMeta(source = "aurn", all = TRUE) #Import site data for aurn network
 
+#Filter sites by those that are currently measuring PM10, and are in a Rural Background
 pm10_sites <- filter(aurn_meta, 
                      variable == "PM10",
                      end_date =="ongoing",
                      site_type=="Rural Background") # Search for pm10 sites with ongoing action with rural background
+
 
 site <- importAURN(site = "HM", data_type = "24_hour", year = 2024) # Import data for specific site
 
